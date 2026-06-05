94c65885a4f74e4e2f40b3882ae878a3b253f473
Brandon Cordell <brandon@brandoncordell.com>
2026-06-04 21:10:11 -0400

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,14 @@
 class ApplicationController < ActionController::Base
   include Authentication
 
-  allow_browser versions: :modern # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  stale_when_importmap_changes # Changes to the importmap will invalidate the etag for HTML responses
+  allow_browser versions: :modern
+  stale_when_importmap_changes
 
-  before_action :check_setup_state
+  prepend_before_action :check_setup_state
 
   private
 
   def check_setup_state
-    return unless User.none?
-
-    redirect_to setup_path
+    redirect_to setup_path if User.none?
   end
 end
--- app/controllers/setup_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Setup controller
-class SetupController < ApplicationController
-  allow_unauthenticated_access
-
-  def index; end
-end
